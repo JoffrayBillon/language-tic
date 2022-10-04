f3939c3ea6b169ccfe38eaf2f0d29a1b3c6852cd
j.billon <j.billon@groupeonepoint.com>
2022-10-04 15:17:05 +0200

    Init language tic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, set } from "./redux/counter/counterSlice";
 
 function App() {
+  const [setter, setSetter] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Typography variant="h1">Counter language tic</Typography>
+      <Typography variant="h2">{count} stick</Typography>
+      <Button onClick={() => dispatch(increment())}>Add point</Button>
+      <Container>
+        <TextField
+          type={"number"}
+          value={setter}
+          onChange={(event) => setSetter(event.target.value)}
+        ></TextField>
+        <Button
+          onClick={() => {
+            dispatch(set(setter));
+            setSetter(0);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Set point
+        </Button>
+      </Container>
+    </Container>
   );
 }
 
